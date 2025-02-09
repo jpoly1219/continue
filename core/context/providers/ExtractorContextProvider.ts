693002b7dcf651d30785a5e60e95df48ec5fe8ce
jpoly1219 <jpoly1219@gmail.com>
2025-02-08 21:21:58 -0500

    comment out unused code

--- core/context/providers/ExtractorContextProvider.ts
+++ core/context/providers/ExtractorContextProvider.ts
@@ -1,4 +1,6 @@
-import { extractContext } from "@jpoly1219/context-extractor";
+// import { extractContext } from "@jpoly1219/context-extractor";
+import { extractContext } from "/home/jacob/projects/context-extractor/dist/src/main";
+
 import { Language } from "@jpoly1219/context-extractor/dist/src/types.js";
 
 import { BaseContextProvider } from "../index.js";
@@ -10,16 +12,18 @@ import type {
   LoadSubmenuItemsArgs,
 } from "../../";
 
+// interface ContextExtractorResult {
+//   holeTypes: string[];
+//   relevantTypes: string[];
+//   relevantHeaders: string[];
+// }
 
-interface ContextExtractorResult {
-  holeTypes: string[];
-  relevantTypes: string[];
-  relevantHeaders: string[];
-}
-
-interface ContextExtractor {
-  extractContext(code: string, language: string): Promise<ContextExtractorResult>;
-}
+// interface ContextExtractor {
+//   extractContext(
+//     code: string,
+//     language: string,
+//   ): Promise<ContextExtractorResult>;
+// }
 
 export class ExtractorContextProvider extends BaseContextProvider {
   constructor(options: { [key: string]: any }) {
@@ -42,40 +46,56 @@ export class ExtractorContextProvider extends BaseContextProvider {
     };
   }
 
-
   async getContextItems(
     query: string,
     extras: ContextProviderExtras,
   ): Promise<ContextItem[]> {
-    const ctx = await extractContext(Language.TypeScript, "/home/jacob/projects/small-examples/context-extractor-test/booking/sketch.ts", "/home/jacob/projects/small-examples/context-extractor-test/booking/");
+    console.log("RUNNING");
+    console.log(`query: ${query}`);
+    console.log(
+      `extras: ${(extras.config, extras.embeddingsProvider, extras.fetch, extras.fullInput, extras.ide, extras.llm, extras.reranker, extras.selectedCode)}`,
+    );
+    try {
+      const ctx = await extractContext(
+        Language.TypeScript,
+        "/home/jacob/projects/context-extractor/targets/booking/sketch.ts",
+        "/home/jacob/projects/context-extractor/targets/booking/",
+      );
 
-    const obj = {
-      ...ctx,
-      relevantTypes: Object.fromEntries(ctx!.relevantTypes),
-      relevantHeaders: Object.fromEntries(ctx!.relevantHeaders),
-    };
-    return [
-      {
-        name: "Test Type 1",
-        description: "A dummy type for testing",
-        content: "interface TestType { id: number; name: string; }"
-      },
-      {
-        name: "Test Type 2",
-        description: "Another dummy type for testing",
-        content: "type Status = 'active' | 'inactive' | 'pending';"
-      },
-      {
-        name: "Test Header",
-        description: "A dummy header for testing",
-        content: "import { useState, useEffect } from 'react';"
-      },
-      {
-        name: "CTX",
-        description: "Extracted context.",
-        content: `${JSON.stringify(obj, null, 2)}`
-      }
-    ];
+      const obj = ctx
+        ? {
+            ...ctx,
+            relevantTypes: Object.fromEntries(ctx!.relevantTypes),
+            relevantHeaders: Object.fromEntries(ctx!.relevantHeaders),
+          }
+        : {};
+      return [
+        {
+          name: "Test Type 1",
+          description: "A dummy type for testing",
+          content: "interface TestType { id: number; name: string; }",
+        },
+        {
+          name: "Test Type 2",
+          description: "Another dummy type for testing",
+          content: "type Status = 'active' | 'inactive' | 'pending';",
+        },
+        {
+          name: "Test Header",
+          description: "A dummy header for testing",
+          content: "import { useState, useEffect } from 'react';",
+        },
+        {
+          name: "CTX",
+          description: "Extracted context.",
+          content: `${JSON.stringify(obj, null, 2)}`,
+        },
+      ];
+    } catch (err) {
+      console.log("ERROR");
+      console.log(err);
+      return [];
+    }
   }
 
   async loadSubmenuItems(_args: LoadSubmenuItemsArgs) {
